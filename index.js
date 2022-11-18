f821faa4890d21c3fe666ebad294d1073051b33c
AliceTYu <turaeva.alice@gmail.com>
2022-11-18 22:24:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
+
+
+// Напишите реализацию createTimerAnimator
+// который будет анимировать timerEl
+const createTimerAnimator = (seconds) => {
+
+  if (seconds > 0){
+  const interval = setInterval(() => {
+
+    seconds--;
+    let second = seconds % 60;
+    let minut = Math.floor(seconds / 60) % 60;
+    let hour = Math.floor(seconds / 60 /60);
+    
+    if(minut <= 9) minut = "0" + minut;
+    if(second <= 9) second = "0" + second;
+    if(hour <= 9) hour = "0" + hour;
+    
+    timerEl.innerHTML = hour + ":" + minut + ":" + second; 
+
+      if (seconds === 0) {
+        clearInterval(interval);
+    }
+  }, 1000);
+  }
+
+    return (seconds) => {};
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener('input', () => {
+  // Очистите input так, чтобы в значении
+  // оставались только числа
+  // не поняла задания (
+});
+
+buttonEl.addEventListener('click', () => {
+  const seconds = Number(inputEl.value);
+
+  createTimerAnimator(seconds);
+
+  console.log(StrHH, StrMM, StrSS);
+
+  inputEl.value = '';
+});
